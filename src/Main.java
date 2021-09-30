58b3e366600f9b159bff7c6acabe2a159af81d7a
Sergei <mr.sergei82@mail.ru>
2021-10-01 00:52:04 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,10 @@
+import java.util.concurrent.Callable;
+
 public class Main {
+    public static void main(String[] args){
+        Course c = new Course("пр1","пр2");
+        Team team = new Team("Ivan","Diman","Roman","Stepan");
+        c.doIt(team);
+        team.showResults();
+    }
 }
---
+++ src/Team.java
@@ -0,0 +1,8 @@
+public class Team {
+    public Team(String m1,String m2,String m3,String m4 ){
+    }
+    public static void showResults(){
+        System.out.println();
+    }
+
+}
