c3d96c6d728b89548b36c19035c3b01d7753be7e
Sergei <mr.sergei82@mail.ru>
2021-10-02 12:59:04 +0300

    1

--- src/DZ2/Main.java
+++ src/DZ2/Main.java
@@ -4,8 +4,9 @@ import java.lang.reflect.Array;
 
 public class Main {
     public static void main(String[] args){
+        String[][] nArr = new String[4][4];
         try {
-        MyArray.myArr();
+            MyArray.myArr(nArr);
         }catch (MyArraySizeException e){
 
         }catch (MyArrayDataException e){
--- src/DZ2/MyArray.java
+++ src/DZ2/MyArray.java
@@ -4,7 +4,7 @@ import java.lang.reflect.Array;
 
 public  class MyArray {
 
-    public static void myArr() throws MyArraySizeException, MyArrayDataException{
+    public static void myArr(String[][] a) throws MyArraySizeException, MyArrayDataException{
         int summ = 0;
         String[][] arr = new String[4][4];
         for (int i = 0; i < arr.length; i++) {
